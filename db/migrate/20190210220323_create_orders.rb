032e71cc19dc95cdd09b723a251fe65060b1736c
SmartBitsApps <gapparena@gmail.com>
2019-02-10 22:05:18 +0000

    adds orders and order items

---
+++ db/migrate/20190210220323_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.string :first_name
+      t.decimal :sub_total, precision: 15, scale: 2, null: false
+
+      t.timestamps
+    end
+  end
+end
