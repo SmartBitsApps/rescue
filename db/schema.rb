0c749428122de8eb5240b0de863d1e995a5769fe
SmartBitsApps <gapparena@gmail.com>
2019-02-11 13:06:54 +0000

    login page - complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_220601) do
+ActiveRecord::Schema.define(version: 2019_02_10_222921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_02_10_220601) do
     t.decimal "sub_total", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
   end
 
   create_table "product_categories", force: :cascade do |t|
