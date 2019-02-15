81b999a54fd39dabee44c686ba29df321657764d
SmartBitsApps <gapparena@gmail.com>
2019-02-15 16:28:58 +0000

    adds working vendor folder tree

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,18 +1,24 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
 # Add vendor paths
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
 
 # Precompile core stylesheets
 Rails.application.config.assets.precompile += [
   #"stylesheets/bootstrap.css",
-  "stylesheets/theme-air.css",
-  "stylesheets/appwork.css",
-  "stylesheets/theme-corporate.css",
-  "stylesheets/colors.css",
-  "stylesheets/uikit.css"
+  #"stylesheets/theme-air.css",
+  #"stylesheets/appwork.css",
+  #"stylesheets/theme-corporate.css",
+  #"stylesheets/colors.css",
+  #"stylesheets/uikit.css"
 ]
 
 # Precompile core javascripts
 Rails.application.config.assets.precompile += ['javascripts/*.js']
+Rails.application.config.assets.precompile += ['javascripts/extra_pages/*.js']
 
 # Precompile fonts
 Rails.application.config.assets.precompile += ['fonts/*']
@@ -24,6 +30,9 @@ lib_files.map! { |file| file.sub(%r(\.(scss)), '.css') }
 Rails.application.config.assets.precompile += lib_files
 
 # Precompile pages
+Rails.application.config.assets.precompile += ['stylesheets/*.css']
+Rails.application.config.assets.precompile += ['stylesheets/pages/*.css']
+Rails.application.config.assets.precompile += ['stylesheets/extra_pages/*.css']
 #Rails.application.config.assets.precompile += ['extras/css/*.css', 'extras/js/*.js']
 
 # Add additional assets to the asset load path.
